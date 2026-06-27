6a50a45fcc9c770f9371299ed19256521710aefd
authoi-sudo <authoiproff@gmail.com>
2026-06-27 19:08:02 +0600

    Create README.cpp

---
+++ README.cpp
@@ -0,0 +1,13 @@
+# cpp-prac-1
+#include<iostream>
+using namespace std ;
+
+int main()
+{ 
+   double num1=10.64655;
+   float num2=13.74423568;
+    char ch1 = 'a' ;
+   cout <<"number 1="<<num1<<endl<<"number 2="<<num2<<endl<<"character="<<ch1;
+  
+   return 0;
+}
